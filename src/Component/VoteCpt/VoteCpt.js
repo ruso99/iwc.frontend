3de2b21d78211bc0bbbdf00ad9173a4bcb2fe760
신동철 <ruso@sindongcheol-ui-MacBookPro.local>
2019-10-31 18:51:51 +0900

    투표스크린 레이아웃 구조 다짰음

--- src/Component/VoteCpt/VoteCpt.js
+++ src/Component/VoteCpt/VoteCpt.js
@@ -4,15 +4,34 @@ import { Box, Grid, Paper } from '@material-ui/core';
 
 const styles = (theme) => ({
   root: {
-    height:'100vh'
+    bottom: 0,
+    position: 'absolute',
+    backgroundColor: '#282c34',
+    height: '100%',
+  },
+  spaceTop: {
+    height: 'calc( 100% - 50px - 40vh - 20vh - 56px - 56px )',
   },
   gridSpacing: {
-    margin: '8px'
+    margin: '8px',
+    textAlign: '-webkit-center',
   },
   divStyle: {
     width: '100%',
     display: 'flex',
-    justifyContent: 'center',
+  },
+  addMobStyle: {
+    width: '320px',
+    height: '50px',
+  },
+  displayStyle: {
+    height: '40vh'
+  },
+  pictureStyle: {
+    height: '20vh'
+  },
+  footerStyle: {
+    height: '56px'
   }
 });
 
@@ -22,22 +41,36 @@ class VoteCpt extends Component {
 
     return (
       <Box className = {classes.root}>
+        <div className = {classes.spaceTop}/>
         <Grid container spacing={0}>
-          <Grid item xs={12} className={classes.gridSpacing}>
-            <Paper>전광판</Paper>
+          <Grid
+            item xs={12}
+            className={classes.gridSpacing}
+          >
+            <Paper className={classes.displayStyle}>전광판</Paper>
           </Grid>
-          <Grid item xs={12} className={classes.gridSpacing}>
-            <Paper>광고</Paper>
+          <Grid
+            item xs={12}
+            className={classes.gridSpacing}
+          >
+            <Paper className={classes.addMobStyle}>광고</Paper>
           </Grid>
           <div className={classes.divStyle}>
-            <Grid item xs={6} className={classes.gridSpacing}>
-              <Paper>사진</Paper>
+            <Grid
+              item xs={6}
+              className={classes.gridSpacing}
+            >
+              <Paper className={classes.pictureStyle}>사진</Paper>
             </Grid>
-            <Grid item xs={6} className={classes.gridSpacing}>
-              <Paper>사진</Paper>
+            <Grid
+              item xs={6}
+              className={classes.gridSpacing}
+            >
+              <Paper className={classes.pictureStyle}>사진</Paper>
             </Grid>
           </div>
         </Grid>
+        <Box className={classes.footerStyle} />
       </Box>
     );
   }
